c8661c444ca571ab56a068998f08ad6fe54e45b3
kwang40 <kaishenw1995@gmail.com>
2019-03-04 12:26:22 -0600

    add check for failures

--- getValidDomains.py
+++ getValidDomains.py
@@ -1,8 +1,11 @@
 import sys
 
+errorStr = '\"http\":{}},\"error\":'
 with open(sys.argv[1]) as infile:
     for line in infile:
         if line[:7] != '{\"ip\":\"':
+            continue
+        if line.find(errorStr) != -1:
             continue
         domainNameStart = line.find('domain') + 9
         domainNameEnd = line.find('\"', domainNameStart)
