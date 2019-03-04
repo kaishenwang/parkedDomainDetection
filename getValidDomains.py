8eeaf5d4652240e255a0deb4eb10f58a47af423f
Kaishen Wang <kwang40@castleblack.sprai.org>
2019-03-04 11:52:57 -0600

    fix bug

--- getValidDomains.py
+++ getValidDomains.py
@@ -4,4 +4,4 @@ with open(sys.argv[1]) as infile:
     for line in infile:
         domainNameStart = line.find('domain') + 9
         domainNameEnd = line.find('\"', domainNameStart)
-        print (domain)
+        print (line[domainNameStart:domainNameEnd])
