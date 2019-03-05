c04b7a87af70db6e84ffc075b68c527ebd8fee4c
kwang40 <kaishenw1995@gmail.com>
2019-03-05 11:48:15 -0600

    fix some bug

--- findOldIDStr.py
+++ findOldIDStr.py
@@ -3,6 +3,7 @@ import re
 
 #python findOldIDStr.py oldIDStr.txt /data1/nsrg/kwang40/fullData/2019-03-03/banners.json
 IDs = {}
+IDStrs = {}
 errorStr = '\"http\":{}},\"error\":'
 def parse(page):
     if page.find(errorStr) != -1:
@@ -13,21 +14,31 @@ def parse(page):
     for ID in IDs.keys():
         if re.match(ID, page, flags=0) != None:
             IDs[ID].append(domainName)
+    for ID in IDStrs.keys():
+        if page.find(ID) != -1:
+            IDStrs[ID].append(domainName)
 
-def writeResult(fName, d):
+def writeResult(fName, d, d2):
     with open(fName, 'w') as f:
         for k,v in d:
-            f.write(k + '(' + len(v) + '):' )
+            f.write(k + '(' + str(len(v)) + '):' )
+            for idx in range(len(v)-1):
+                f.write(v[idx] + ',')
+            f.write(v[-1] + '\n')
+        f.write('\n')    
+        for k,v in d2:
+            f.write(k + '(' + str(len(v)) + '):' )
             for idx in range(len(v)-1):
                 f.write(v[idx] + ',')
             f.write(v[-1] + '\n')
 
-
 with open (sys.argv[1]) as f:
     lines = f.readlines()
 
-for line in lines:
-    IDs[line.rstrip()] = []
+for i in range(5):
+    IDs[lines[i].rstrip()] = []
+for i in range(5, len(lines)):
+    IDStrs[lines[i].rstrip()] = []
 
 singlePage = ''
 with open(sys.argv[1]) as infile:
@@ -42,6 +53,9 @@ parse(singlePage)
 
 for k,v in IDs.items():
     IDs[k] = list(set(v))
+for k,v in IDStrs.items():
+    IDStrs[k] = list(set(v))
 
 sorted_list = sorted(IDs.items(), key=lambda x: len(x[1]), reverse=True)
-writeResult('oldIDMatchResult.txt', sorted_list)
+sortedStr_list = sorted(IDs.items(), key=lambda x: len(x[1]), reverse=True)
+writeResult('oldIDMatchResult.txt', sorted_list, sortedStr_list)
