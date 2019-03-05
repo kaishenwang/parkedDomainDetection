b1ad743e24950e9dc8766422f36b392b4d35b141
kwang40 <kaishenw1995@gmail.com>
2019-03-05 17:44:31 -0600

    not use regex

--- findURLInParkedDomainPages.py
+++ findURLInParkedDomainPages.py
@@ -6,21 +6,30 @@ URLs = defaultdict(list)
 endingChars = [')', '\"', ' ', '=', '\\', '}']
 errorStr = '\"http\":{}},\"error\":'
 
+def find_all(a_str, sub):
+    start = 0
+    while True:
+        start = a_str.find(sub, start)
+        if start == -1: return
+        yield start
+        start += len(sub)
+
 def parse(page):
     if page.find(errorStr) != -1:
         return
     domainNameStart = page.find('domain') + 9
     domainNameEnd = page.find('\"', domainNameStart)
     domainName = page[domainNameStart : domainNameEnd]
-    urlIdxs = [m.start() for m in re.finditer('http://', page)]
-    for urlIdx in urlIdxs:
-        for i in range(7, 30):
-            endIdx = url + i
-            if endIdx >= len(page):
-                break
-            if page[endIdx] in endingChars:
-                break
-        URLs[page[urlIdx : endIdx]].append(domainName)
+    for beginStr in ['http://','https://']:
+        urlIdxs = find_all(beginStr, page)
+        for urlIdx in urlIdxs:
+            for i in range(7, 30):
+                endIdx = url + i
+                if endIdx >= len(page):
+                    break
+                if page[endIdx] in endingChars:
+                    break
+            URLs[page[urlIdx : endIdx]].append(domainName)
 
 def writeResult(fName, d):
     with open(fName, 'w') as f:
@@ -44,4 +53,4 @@ parse(singlePage)
 for k,v in URLs.items():
     URLs[k] = list(set(v))
 sorted_list = sorted(URLs.items(), key=lambda x: len(x[1]), reverse=True)
-writeResult('newIDURL', sorted_list)
+writeResult('newIDURL.txt', sorted_list)
