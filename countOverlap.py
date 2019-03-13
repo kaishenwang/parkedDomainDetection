00b77251192de3e81d13fc1fab29bcdb151c3520
kwang40 <kaishenw1995@gmail.com>
2019-03-13 11:49:39 -0500

    add script

---
+++ countOverlap.py
@@ -0,0 +1,39 @@
+validDomains = {}
+with open('validDomains.txt') as f:
+    for line in f:
+        validDomains[line.rstrip()] = True
+
+oldDomains = {}
+# Token NS
+nonValidNS = {}
+with open('nonValidTokenNS.txt') as f:
+    lines = f.readlines()
+for line in lines:
+    nonValidNS[line.rstrip()] = True
+
+with open('tokenNS.txt') as f:
+    lines = f.readlines()
+for line in lines:
+    parts = line.rstrip().split(':')
+    if parts[0] in nonValidNS:
+        continue
+    domainsList = parts[1].split(',')
+    for domain in domainsList:
+        if domain in validDomains:
+            oldDomains[domain] = True
+
+with open('domainsByOldUsefulID.txt') as f:
+    for line in f:
+        domain = line.rstrip()
+        if domain in validDomains:
+            oldDomains[domain] = True
+
+print('Old Domains: ' + str(len(oldDomains)))
+
+overLapCount = 0
+with open('domainsByNewUsefulID.txt') as f:
+    for line in f:
+        domain = line.rstrip()
+        if domain in oldDomains:
+            overLapCount += 1
+print ('Overlap count is ' + overLapCount)
